e9abafa43f7ef9da29f1bff55a53e9fa2b65b85f
RomanWandelow <romanwandelow@gmail.com>
2024-05-19 07:56:09 -0300

    mi primer commit

---
+++ funcion.js
@@ -0,0 +1,11 @@
+/*const cambiarFondoBody = document.getElementById('bodyId');
+const bodyClick =addEventListener('click', function(){
+ const colores = [" #958689;", "blue", "green", "orange", "purple"];
+const cambiarColor = colores[Math.floor(Math.random() * colores.length)]
+cambiarFondoBody.style.backgroundColor = cambiarColor
+})
+
+
+const nombre = document.querySelector ('[for = nombre]')
+const apellido = document.querySelector ('[for = apellido]')
+
